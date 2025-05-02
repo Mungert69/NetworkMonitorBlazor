36fb4d04df6e2b44c14f28f3a0929354812188f5
Mungert69 <contact@mahadeva.co.uk>
2025-05-02 05:13:07 +0200

    ,

--- Program.cs
+++ Program.cs
@@ -65,7 +65,7 @@ builder.Services.AddScoped<WebSocketService>(provider =>
 // Configure Kestrel
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    serverOptions.ListenAnyIP(7860);
+    serverOptions.ListenAnyIP(7861);
 });
 
 var app = builder.Build();
